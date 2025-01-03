e64dfebf0f0ae137e94a00df37812432cdab164d
Eduardogl444 <eduardogl444@gmail.com>
2025-01-02 20:08:05 -0500

    Started the html and css of the landing page

---
+++ client/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css'; 
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      <h1>MovieMern</h1>
+      <h2>Explore the world of Cineama!</h2>
+      <p>Welcome to MovieMern, your ultimate movie guide! Dive into a vast library of films from blockbusters to indie gems. Search, rate, and keep track of everything you've watched and loved. Ready to start your cinematic journey? Click below to sign in and get started today.</p>
+      <button className="begin-button">Let's Begin</button>
+    </div>
+  );
+}
+
+export default LandingPage;
