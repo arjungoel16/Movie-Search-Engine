63737dd46131f14854db3b04fef708fd2926ef0b
arjungoel16 <arjun.goel6@gmail.com>
2025-01-06 17:44:55 -0500

    Create mut.js
    functions -> remove movie, save movie, add user, login user

---
+++ client/src/utils/mut.js
@@ -0,0 +1,67 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// saves movies within the users space
+export const SAVE_Movie = gql`
+  mutation saveMovie(
+    $authors: [String]!
+    $description: String!
+    $title: String!
+    $movieId: ID!
+    
+  ) {
+     saveMovie(
+    // Directors: $authors
+    // description: $description
+      title: $title
+      movieId: $movieId
+    ) {
+      _id
+      username
+    
+      savedMovie {
+        movieId
+        title
+      }
+    }
+  }
+`;
+
+
+// function for removing movies from the users space
+export const REMOVE_MOVIE = gql`
+  mutation removeMovie($MovieId: ID!) {
+    removeMovie(MovieId: $MovieId) {
+      _id
+      username
+      
+      savedMovie {
+        movieId
+        title
+      }
+    }
+  }
+`;
