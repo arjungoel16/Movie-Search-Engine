2adec5c974c881b459f60900c4424e37c12bbdda
arjungoel16 <arjun.goel6@gmail.com>
2025-01-06 19:57:49 -0500

    Update SearchMovies.js

--- client/src/pages/SearchMovies.js
+++ client/src/pages/SearchMovies.js
@@ -53,7 +53,7 @@ const SearchMovies = () => {
       const movieData = items.map((book) => ({
         movieId: movie.id,
         title: book.volumeInfo.title,
-        Director: book.volumeInfo.Director || ["No Director to display"],
+        // Director: book.volumeInfo.Director || ["No Director to display"],
       }));
 
       setSearchedMovie(movieData);
