b76a497893f3b33e2fc95878191d7ff3fbbb6586
arjungoel16 <arjun.goel6@gmail.com>
2025-01-12 00:56:48 -0500

    Update SavedMovies.tsx

--- client/src/pages/SavedMovies.tsx
+++ client/src/pages/SavedMovies.tsx
@@ -1,150 +1,45 @@
-    import React from "react";
-    import {
-    Jumbotron, //this one is no longer in the react library
-    Container,
-    CardColumns, //this one is no longer in the react library
-    Card,
-    Button,
-    } from "react-bootstrap";
-    import { useQuery, useMutation } from "@apollo/client";
-//     import React from "react";
-//     import {
-//     Jumbotron,
-//     Container,
-//     CardColumns,
-//     Card,
-//     Button,
-//     } from "react-bootstrap";
-//     import { useQuery, useMutation } from "@apollo/client";
+// Purpose: Provide a page for the user to view their saved movies and delete them from their saved list.
+//  do not use server to get saved movies
 
-//     import Auth from "../utils/auth";
-//     import { removeMovieId } from "../utils/localStorage";
-//     import { GET_ME } from "../utils/queries";
-//     import {REMOVE_MOVIE} from "../utils/mutations";
-//     const SaveMovies = () => {
-//     const { loading, data } = useQuery(GET_ME);
-//     // create state to hold saved movieId values
-//     const userData = data?.me;
-//     const [deleteMovie] = useMutation(REMOVE_MOVIE);
-//     const [removeMovieId] = useMutation(REMOVE_MOVIE_ID);
-// // create function that accepts the movie's mongo _id value as param and deletes the movie from the database
-//     const handleDeleteMovie = async (movieId) => {
-//     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-//       if (!token) {
-//         return false;
-//       }
-
-//       try {
-//         await deleteMovie({
-//           variables: {movieId: movieId},
-//         });
-//         // when the data is updated, we delete the movie from the local storage
-//         removeBookId(movieId);
-//       } catch (err) {
-//         console.error(err);
-//       }
-//     };
-//     if (loading) {
-//       return <h2>LOADING...</h2>;
-//     }
-
-//     return (
-//       <>
-//         <Jumbotron fluid className="text-light bg-dark">
-//           <Container>
-//             <h1>Viewing saved books!</h1>
-//           </Container>
-//         </Jumbotron>
-//         <Container>
-//           <h2>
-//             {userData.savedBooks.length
-//               ? `Viewing ${userData.savedBooks.length} saved ${
-//                   userData.savedBooks.length === 1 ? "book" : "books"
-//                 }:`
-//               : "You have no saved movies!"}
-//           </h2>
-//           <CardColumns>
-//             {userData.SaveMovies.map((movie) => {
-//               return (
-//                 <Card key={movie.movieId} border="dark">
-//                   {movie.image ? (
-//                     <Card.Img
-//                       src={movie.image}
-//                       alt={`The cover for ${movie.title}`}
-//                       variant="top"
-//                     />
-//                   ) : null}
-//                   <Card.Body>
-//                     <Card.Title>{movie.title}</Card.Title>
-//                     <p className="small">Directors: {movie.Director}</p>
-//                     <Card.Text>{movie.description}</Card.Text>
-//                     <Button
-//                       className="btn-block btn-danger"
-//                       onClick={() => handleDeleteMovie(movie.movieId)}
-//                     >
-//                       Delete this movie!
-//                     </Button>
-//                   </Card.Body>
-//                 </Card>
-//               );
-//             })}
-//           </CardColumns>
-//         </Container>
-//       </>
-//     );
-//   };
-//   export default SaveMovies;
-
-
-// import { useState, useEffect } from 'react';
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
-// import { getMe, removeBook } from '../utils/API';
-import { GET_ME } from '../utils/queries';
+
 import Auth from '../utils/auth';
-import { removeMovieId } from '../utils/localStorage';
-import type { User } from '../models/user';
-import { REMOVE_MOVIE } from '../utils/mutations';
-import { useParams } from 'react-router-dom';
+import { removeMovie } from '../utils/localStorage';
 
 import React from 'react';
+import { GET_ME } from '../utils/queries';
 
 const SavedMovies: React.FC = () => {
-  const { username: userParam } = useParams();
   const { loading, error, data } = useQuery(GET_ME);
-  const [removeMovie] = useMutation(REMOVE_MOVIE, {
-    refetchQueries: [{ query: GET_ME, variables: { username: userParam } }],
-  });
 
   console.log('Query data:', data);
 
-  const userData: User =data?.me || data?.user || {};
+  const userData = data?.me || data?.user || {};
   console.log('User data:', userData);
   console.log('Saved Movies:', userData.savedMovies);
 
-    // The useQuery hook automatically executes when the component mounts and will re-fetch data when necessary, making the useEffect hook unnecessary in this case.
+  // The useQuery hook automatically executes when the component mounts and will re-fetch data when necessary, making the useEffect hook unnecessary in this case.
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteMovie = async (movieId: string) => {
-      const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const handleDeleteMovie = async (movieId: any) => {
+    const token = Auth.logIn() ? Auth.getToken() : null;
 
-      if (!token) {
-        return false;
-      }
+    if (!token) {
+      return false;
+    }
 
-      try {
-        await removeMovie({
-          variables: { movieId },
-        });
+    try {
+      await removeMovie({
+        variables: { movieId },
+      });
 
-        removeMovieId(movieId);
-      } catch (err) {
-        console.error(err);
-            }
-        };
+      removeMovie(movieId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  
   // if data isn't here yet, say so
   if (loading) {
     return <h2>LOADING...</h2>;
@@ -152,7 +47,7 @@ const SavedMovies: React.FC = () => {
   if (error) {
     console.error(error);
     // return <h2>Something went wrong!</h2>;
-    return <h2>Error: {error.message}</h2>
+    return <h2>Error: {error.message}</h2>;
   }
 
   return (
@@ -176,10 +71,10 @@ const SavedMovies: React.FC = () => {
         </h2>
         <Row>
           {/* {userData?.savedBooks?.map((book: { bookId: string; image?: string; title: string; authors: string[]; description: string }) => { */}
-          {userData.savedMovies.map((movie) => {    //changed to check for savedBooks
+          {userData.savedMovies.map((movie: any) => {
             return (
               <Col md='4'>
-                <Card key={movie.movieId} border='dark'>
+                <Card key={movie} border='dark'>
                   {movie.image ? (
                     <Card.Img
                       src={movie.image}
@@ -201,11 +96,11 @@ const SavedMovies: React.FC = () => {
                 </Card>
               </Col>
             );
-          })}
+          }
+          )}
         </Row>
       </Container>
     </>
   );
-};
-
+}
 export default SavedMovies;
