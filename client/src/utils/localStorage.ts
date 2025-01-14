c7ccb35264ba40d953a75faa00426e34e6a9e8eb
arjungoel16 <arjun.goel6@gmail.com>
2025-01-13 20:22:30 -0500

    Update localStorage.ts

--- client/src/utils/localStorage.ts
+++ client/src/utils/localStorage.ts
@@ -1,27 +1,33 @@
+import SavedMovies from "../pages/SavedMovies";
+
 export const getSavedMovieIds = () => {
-  // allows us to store the movie
+  // allows us to retrieve the movie
   const savedMovieIds = localStorage.getItem('save_movies')
-    ? JSON.parse(localStorage.getItem('save_movies'))
+    ? JSON.parse(localStorage.getItem('save_movies') || '[]')
     : [];
+  // const savedMovieIds = localStorage.getItem('save_movies')
+  //   ? JSON.parse(localStorage.getItem('save_movies'))
+  //   : [];
 
   return savedMovieIds;
 };
 
 
 // allows us to export the movie ids
-export const saveMovieIds = (MovieIdArr) => {
+export const saveMovieIds = (MovieIdArr: any) => {
   if (MovieIdArr.length) {
-    
-    localStorage.setItem('save_movie', JSON.stringify(MovieIdArr));
+
+    localStorage.setItem('save_movies', JSON.stringify(MovieIdArr));
   } else {
     // if there is no movie name, remove it
     localStorage.removeItem('save_movies');
   }
 };
+// allows us to remove the movie
 
-export const removeBookId = (bookId) => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
+export const removeMovie = (movieId: any) => {
+  const savedBookIds = localStorage.getItem('saved_movies')
+    ? JSON.parse(localStorage.getItem('saved_movies') || 'null')
     : null;
 
   if (!savedBookIds) {
@@ -29,8 +35,9 @@ export const removeBookId = (bookId) => {
   }
 
   // updating the movies in the local storage
-  const updatedSavedMovieIds = savedMovieIds?.filter((savedMovieId) => savedMovieId !== movieId);
-  localStorage.setItem('save_movies', JSON.stringify(updatedSavedMovieIds));
 
-  return true;
-};
+  const updatedSavedMovieIds: string[] = savedBookIds?.filter((savedMovieId: string) => savedMovieId !== movieId);
+    localStorage.setItem('save_movies', JSON.stringify(updatedSavedMovieIds));
+    return true;
+  };
+
