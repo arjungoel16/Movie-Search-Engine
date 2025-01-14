7d016556e4b7031214f9e365e09d4fbe690febe9
seanfdolan <sean.f.dolan@gmail.com>
2025-01-13 20:36:56 -0500

    local storage

--- client/src/utils/localStorage.ts
+++ client/src/utils/localStorage.ts
@@ -14,14 +14,8 @@ export const getSavedMovieIds = () => {
 
 
 // allows us to export the movie ids
-<<<<<<< HEAD
 export const saveMovieIds = (MovieIdArr: string[]) => {
   if (MovieIdArr.length) {
-=======
-export const saveMovieIds = (MovieIdArr: any) => {
-  if (MovieIdArr.length) {
-
->>>>>>> ecae1304d75aff1089d98a1e4cbcc274dac87bd8
     localStorage.setItem('save_movies', JSON.stringify(MovieIdArr));
   } else {
     // if there is no movie name, remove it
@@ -30,15 +24,9 @@ export const saveMovieIds = (MovieIdArr: any) => {
 };
 // allows us to remove the movie
 
-<<<<<<< HEAD
 export const removeMovieId = (movieId) => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
-=======
-export const removeMovie = (movieId: any) => {
   const savedBookIds = localStorage.getItem('saved_movies')
-    ? JSON.parse(localStorage.getItem('saved_movies') || 'null')
->>>>>>> ecae1304d75aff1089d98a1e4cbcc274dac87bd8
+    ? JSON.parse(localStorage.getItem('saved_movies'))
     : null;
 
   if (!savedBookIds) {
